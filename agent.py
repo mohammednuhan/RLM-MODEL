0cfd3ab342b5fc11d61979209b6339887bba4f10
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-09-09 19:25:29 +0530

    rlm model

---
+++ agent.py
@@ -0,0 +1,24 @@
+import dotenv
+from openai import Openai
+from rich import rich 
+
+load_env() 
+
+input = user_input()
+
+model = client (
+    model = "https",
+    client= "OPENROUTER_KEY"
+)
+
+while true :
+    
+    if :
+    input = user_input ( "You >")
+    continue :
+
+    else : 
+        user_input = "/exit"
+        print ( "bye bye ")
+
+
